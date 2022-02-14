a40da98335db8506c934e25778c9c8d8b1f59253
Samuel C. Brothers <55204767+Sbrother237@users.noreply.github.com>
2022-02-14 15:37:46 -0500

    Completed Medium 3
    Completed challenge 3 in the Medium folder

--- content/challenges/medium/3 - Remove Characters/func.js
+++ content/challenges/medium/3 - Remove Characters/func.js
@@ -1,4 +1,8 @@
 // Create a function that takes a string, removes all "special" characters (e.g. !, @, #, $, %, ^, &, \, *, (, )) and 
 // returns the new string. The only non-alphanumeric characters allowed are dashes -, underscores _ and spaces.
 module.exports = (str) => {
+
+    const removeChar = str.replace(/[^a-zA-Z0-9-_. ]/g, '');
+
+    return removeChar;
 }
