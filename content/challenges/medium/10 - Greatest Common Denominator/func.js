53f1cc74735fbe063b3e1c7a03e21bc6a50403fb
Samuel C. Brothers <55204767+Sbrother237@users.noreply.github.com>
2022-02-21 19:27:47 -0500

    3 More Medium Challenges Complete
    Medium Challenges #5, #9 and #10 complete.

--- content/challenges/medium/10 - Greatest Common Denominator/func.js
+++ content/challenges/medium/10 - Greatest Common Denominator/func.js
@@ -3,4 +3,19 @@
 //   - Both values will be positive.
 //   - The GCD is the largest factor that divides both numbers.
 module.exports = (int1, int2) => {
+
+    if (int1 == int2) {
+        return int2;
+    } 
+
+    while (int2 != 0) {
+        if (int1 > int2) {
+            int1 = int1 - int2;
+        } else {
+            int2 = int2 - int1;
+        }
+    }
+
+    return int1;
+
 }
--- content/challenges/medium/9 - Palindrome/func.js
+++ content/challenges/medium/9 - Palindrome/func.js
@@ -2,4 +2,13 @@
 // Notes: 
 // An empty string counts as a palindrome.
 module.exports = (str) => {
+
+    let re = /[\W]/g;
+
+    let lowCaseStr = str.toLowerCase().replace(re, '');
+
+    let reverseStr = lowCaseStr.split('').reverse().join('');
+
+    return reverseStr === lowCaseStr;
+
 }
